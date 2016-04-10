7e91074febf53092bc56776e01ff748e549e805c
Leo <leonardo@justcode.com.br>
2016-04-10 19:20:44 -0300

    Xamarin Forms

---
+++ PMA.Forms/PMA.Forms/App.cs
@@ -0,0 +1,17 @@
+using MvvmCross.Platform.IoC;
+
+namespace PMA.Forms
+{
+    public class App : MvvmCross.Core.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<ViewModels.FirstViewModel>();
+        }
+    }
+}
